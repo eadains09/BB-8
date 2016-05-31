4e513dfd4c7c1d3750fa99a90c782bf33c2c5604
eadains09 <eadains09@gmail.com>
2016-05-30 22:19:51 -0700

    changing run loop to serial read and parse strings

--- Actual_Code/BB-8_Drive/BB-8_Drive.ino
+++ Actual_Code/BB-8_Drive/BB-8_Drive.ino
@@ -1,5 +1,6 @@
 // BB-8 Motor and Bluetooth Communication AI
 // HC-06 bluetooth module name, 1234 pairing key
+// TODO: We're going to need a failsafe that cuts motors when bluetooth disconnects
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
@@ -9,7 +10,7 @@ Adafruit_DCMotor* motors[] = {AFMS.getMotor(1), AFMS.getMotor(2)};
 float differential;
 int motorSpeed;
 
-int inputState;
+String input;
 int inputHandled;
 
 void setup() {
@@ -21,77 +22,26 @@ void setup() {
 void loop() {
   // Get current command state  
   if(Serial.available() > 0) {
-    inputState = Serial.read();
+    input = Serial.readString();
     inputHandled = 0;
   }
   
-  // Based on inputState call directional functions
+  // Based on input call directional functions
   if (!inputHandled) {
-    switch (inputState) {
-      case '0':
-        setMotorSpeed(0);
-        break;
-      case '1':
-        setMotorSpeed(25);
-        break;
-      case '2':
-        setMotorSpeed(50);
-        break;
-      case '3':
-        setMotorSpeed(75);
-        break;
-      case '4':
-        setMotorSpeed(100);
-        break;
-      case '5':
-        setMotorSpeed(125);
-        break;
-      case '6':
-        setMotorSpeed(150);
-        break;
-      case '7':
-        setMotorSpeed(175);
-        break;
-      case '8':
-        setMotorSpeed(200);
-        break;
-      case '9':
-        setMotorSpeed(225);
-        break;
-      case 'a':
-        incrementSpeed();
-        break;
-      case 'd':
-        decrementSpeed();
-        break;
-      case 'f':
-        setDirection(FORWARD);
-        break;
-      case 'b':
-        setDirection(BACKWARD);
-        break;
-      case 'r':
-        setDifferential(-0.5);
-        break;
-      case 'l':
-        setDifferential(0.5);
-        break;
-      case 't':
-        setDifferential(0);
-        break;
-      case 'h':
-        incrementDifferential();
-        break;
-      case 's':
-        decrementDifferential();
-        break;
-      case 'p':
-        reset();
-        break;
-      default:
-        Serial.println("Command Not Found.");
-        break;
+    int commaIndexA = input.indexOf(',');
+    int commaIndexB = input.indexOf(',', commaIndexA + 1);
+    int dirInput = input.substring(0, commaIndexA).toInt();
+    int speedInput = input.substring(commaIndexA + 1, commaIndexB).toInt();
+    float diffInput = input.substring(commaIndexB + 1).toFloat();
+    
+    if (dirInput) {
+      setDirection(FORWARD);
+    } else {
+      setDirection(BACKWARD);
     }
+    setDifferential(diffInput);
+    setMotorSpeed(speedInput);
+//    handleTextualInput(input[0]);
     inputHandled = 1;
   }
 }
@@ -99,7 +49,6 @@ void loop() {
 void reset() {
   differential = 0;
   motorSpeed = 0;
-  inputState = 0; // Motor Stop
   inputHandled = 1; // Initialized to input has been handled
   setDirection(FORWARD);
 }
@@ -173,4 +122,72 @@ void incrementSpeed() {
 
 void decrementSpeed() {
   setMotorSpeed(motorSpeed - 1);
+}
+
+void handleTextualInput(char inputState) {
+  switch (inputState) {
+      case '0':
+        setMotorSpeed(0);
+        break;
+      case '1':
+        setMotorSpeed(25);
+        break;
+      case '2':
+        setMotorSpeed(50);
+        break;
+      case '3':
+        setMotorSpeed(75);
+        break;
+      case '4':
+        setMotorSpeed(100);
+        break;
+      case '5':
+        setMotorSpeed(125);
+        break;
+      case '6':
+        setMotorSpeed(150);
+        break;
+      case '7':
+        setMotorSpeed(175);
+        break;
+      case '8':
+        setMotorSpeed(200);
+        break;
+      case '9':
+        setMotorSpeed(225);
+        break;
+      case 'a':
+        incrementSpeed();
+        break;
+      case 'd':
+        decrementSpeed();
+        break;
+      case 'f':
+        setDirection(FORWARD);
+        break;
+      case 'b':
+        setDirection(BACKWARD);
+        break;
+      case 'r':
+        setDifferential(-0.5);
+        break;
+      case 'l':
+        setDifferential(0.5);
+        break;
+      case 't':
+        setDifferential(0);
+        break;
+      case 'h':
+        incrementDifferential();
+        break;
+      case 's':
+        decrementDifferential();
+        break;
+      case 'p':
+        reset();
+        break;
+      default:
+        Serial.println("Command Not Found.");
+        break;
+    }
 }
